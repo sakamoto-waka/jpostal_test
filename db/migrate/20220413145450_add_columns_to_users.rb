8439d2dca98dd2172dd221feebf49676ad2b1b1a
sakamoto-waka <wakanmhl@gmail.com>
2022-04-13 14:56:43 +0000

    [update] マイグレーションファイル　カラム追加

--- db/migrate/20220413145450_add_columns_to_users.rb
+++ db/migrate/20220413145450_add_columns_to_users.rb
@@ -1,5 +1,9 @@
 class AddColumnsToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :postcode, :integer
+    add_column :users, :prefecture_code, :integer
+    add_column :users, :address_city, :string
+    add_column :users, :address_street, :string
+    add_column :users, :address_building, :string
   end
 end
