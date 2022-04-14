ed766ca3e2e17d8c05327613717c211e5fd7ee07
sakamoto-waka <wakanmhl@gmail.com>
2022-04-14 13:21:58 +0000

    [add] jqiery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,24 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require
+
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: 'popper.js'
+  })
+  ('#user_postcode').jpostal({
+    postcode: ['#user_postcode'],
+    address: {
+      '#user_prefecture_code': '%3',
+      '#user_address_city': '%4',
+      '#user_address_street': '%5%6%7',
+    },
+  })
+);
+
+
+
 
 module.exports = environment
